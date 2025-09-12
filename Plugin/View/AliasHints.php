927b6237137b693dc2ef6abe6e49fcb4a601facf
Kamil <kamil@kingfisherdirect.co.uk>
2025-09-12 17:19:05 +0200

    Avoid adding debug information to block child HTML if debugger is not enabled

--- Plugin/View/AliasHints.php
+++ Plugin/View/AliasHints.php
@@ -2,15 +2,23 @@
 
 namespace KingfisherDirect\BetterDebugHints\Plugin\View;
 
+use KingfisherDirect\BetterDebugHints\Helper\Config;
 use Magento\Framework\View\Element\AbstractBlock;
 
 class AliasHints
 {
+    private bool $isEnabled;
+
+    public function __construct(Config $config)
+    {
+        $this->isEnabled = $config->isHintEnabled();
+    }
+
     public function aroundGetChildHtml(AbstractBlock $block, \Closure $proceed, string $alias = '', $useCache = true)
     {
         $html = $proceed($alias, $useCache);
 
-        if (!$alias || !$html) {
+        if (!$this->isEnabled || !trim($html)) {
             return $html;
         }
 
@@ -19,8 +27,8 @@ class AliasHints
         $childName = $layout->getChildName($name, $alias);
 
         return
-            "<script type='text/mage-debug' data-mage-debug-position='start' data-mage-debug='$childName'></script>".
-            $html.
+            "<script type='text/mage-debug' data-mage-debug-position='start' data-mage-debug='$childName'></script>" .
+            $html .
             "<script type='text/mage-debug' data-mage-debug-position='end' data-mage-debug='$childName'></script>";
     }
 }
